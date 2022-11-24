dfe634122244d4ed33c1446232210820f9ea34ea
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-11-24 19:54:26 +0000

    Added listener, dir update, dotenv install

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,12 @@ app.use(express.json())
 app.get("/", (req: Request, res: Response) => {
   res.send("T&A API")
 })
+
+app.post("/api/auth/register", (req: Request, res: Response) => {
+  const { name, email, clocknumber, password, repeatPassword } = req.body
+  console.log(name, email, clocknumber, password, repeatPassword)
+
+  res.send("Worked")
+})
+
+app.listen(4000, () => console.log("Server running"))
